2c49d001f6a5a879fcd721e0de1a415b927e57ac
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2024-02-15 06:39:15 -0800

    main

--- WeatherApp/navigation/appNavigation.tsx
+++ WeatherApp/navigation/appNavigation.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/home';
 
 const Stack = createNativeStackNavigator();
 
-export default function StackNavigation () {
-   return (
+export default function StackNavigation() {
+  return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
-   )
+      <Stack.Navigator>
+        <Stack.Screen 
+        options={{ headerShown: false }}
+        name="Home"
+        component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
