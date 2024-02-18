92c43a13427c2ab185081c7c9963f3f9ed9bbf94
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2024-02-18 08:18:55 -0800

    main

--- WeatherApplication/screens/home.tsx
+++ WeatherApplication/screens/home.tsx
@@ -7,142 +7,176 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  StatusBar,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import tw from 'twrnc';
 
 export default function Home() {
-  const [showSearch, setShowSearch] = useState(false);
-  const [location, setLocation] = useState([1, 2, 3]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [searchInput, setSearchInput] = useState('');
 
   let api_key = '843b3585ba284577bc7233248230404';
 
+  const fetchWeatherData = async () => {
+    try {
+      const trimmedSearchInput = searchInput.trim();
+      if (!trimmedSearchInput) {
+        Alert.alert('Please enter a city name.');
+        return;
+      }
+
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${trimmedSearchInput}&days=7&aqi=no&alerts=no`,
+      );
+      if (!response.ok) {
+        throw new Error('Failed to fetch weather data');
+      }
+
+      const data = await response.json();
+      if (data.error) {
+        Alert.alert('City not found. Please enter a valid city name.');
+        return;
+      }
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather data: ', error);
+      Alert.alert('This City Name not Match the API.');
+    }
+  };
+
+  const handleSearch = () => {
+    fetchWeatherData();
+  };
+
   return (
-    <View style={[tw`flex-1`]}>
+    <KeyboardAvoidingView
+      style={[tw`flex-1`]}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <StatusBar hidden />
       <Image
         style={[tw`absolute inset-0 w-full h-full`, {zIndex: -1}]}
         blurRadius={10}
         source={require('../images/background3.jpg')}
       />
-      <SafeAreaView style={[tw`flex flex-1`]}>
-        {/* Search Section */}
-        <View style={[tw`mx-4 relative z-50 h-20 mt-4`]}>
-          <View
-            style={[
-              tw`flex-row justify-between items-center rounded-full pl-4 pr-2`,
-              {backgroundColor: 'rgba(255, 255, 255, 0.2)'},
-            ]}>
-            <TextInput
-              placeholder="Search City"
-              placeholderTextColor="gray"
-              style={[tw`flex-1 text-base text-white`]}
-            />
-            <TouchableOpacity
+      <SafeAreaView style={[tw`flex-1`]}>
+        <ScrollView contentContainerStyle={[tw`flex-grow`]}>
+          <View style={[tw`mx-4 mt-4`]}>
+            <View
               style={[
-                tw`p-3 m-1 rounded-full`,
-                {backgroundColor: 'rgba(0, 0, 0, 0.5)'},
+                tw`flex-row justify-between items-center rounded-full pl-4 pr-2`,
+                {backgroundColor: 'rgba(255, 255, 255, 0.2)'},
               ]}>
-              <Icon name="search" size={25} color="white" />
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        {/* Forecost Section */}
-        <View style={[tw`mx-4 flex justify-around flex-1 mb-2`]}>
-            <Text style={[tw`text-white text-center text-2xl font-bold`]}>London,
-            <Text style={[tw`text-gray-300 text-center text-lg font-semibold`]}>United Kingdom</Text>
-            </Text>
-
-            {/* Weather Image */}
-            <View style={[tw`flex-row justify-center`]}>
-              <Image source={require("../images/partlycloud.png")}
-              style={[tw`w-58 h-52`]}/>
+              <TextInput
+                placeholder="Search City"
+                placeholderTextColor="gray"
+                style={[tw`flex-1 text-base text-white`]}
+                onChangeText={text => setSearchInput(text)}
+                value={searchInput}
+              />
+              <TouchableOpacity
+                style={[
+                  tw`p-3 m-1 rounded-full`,
+                  {backgroundColor: 'rgba(0, 0, 0, 0.5)'},
+                ]}
+                onPress={handleSearch}>
+                <Icon name="search" size={25} color="white" />
+              </TouchableOpacity>
             </View>
+          </View>
 
-            {/* Degree */}
-            <View style={[tw`space-y-2`]}>
-              <Text style={[tw`text-center font-bold text-6xl ml-5 text-white`]}>23°C</Text>
-              <Text style={[tw`text-center text-white text-xl tracking-widest`]}>Partly Cloud</Text>
+          {weatherData && !weatherData.error ? (
+            <View style={[tw`mx-4 mb-2 mt-6`]}>
+              <Text style={[tw`text-white text-center text-4xl font-bold`]}>
+                {weatherData.location.name}
+              </Text>
+              <Text
+                style={[tw`text-gray-400 text-center text-xl font-semibold`]}>
+                {weatherData.location.country}
+              </Text>
+              <View style={[tw`flex-row justify-center`]}>
+                <Image
+                  source={{uri: `https:${weatherData.current.condition.icon}`}}
+                  style={[tw`w-60 h-60`]}
+                />
+              </View>
+              <View style={[tw`space-y-2`]}>
+                <Text
+                  style={[tw`text-center font-bold text-6xl ml-5 text-white`]}>
+                  {weatherData.current.temp_c}°C
+                </Text>
+                <Text
+                  style={[tw`text-center text-white text-xl tracking-widest`]}>
+                  {weatherData.current.condition.text}
+                </Text>
+              </View>
+              <View style={[tw`flex-row justify-between mx-4 my-4`]}>
+                <View style={[tw`flex-row items-center space-x-2`]}>
+                  <Icon name="air" size={30} style={[tw`text-white`]} />
+                  <Text style={[tw`text-white font-semibold text-base mx-1`]}>
+                    {weatherData.current.wind_kph} km/h
+                  </Text>
+                </View>
+                <View style={[tw`flex-row items-center space-x-2`]}>
+                  <Icon name="water-drop" size={30} style={[tw`text-white`]} />
+                  <Text style={[tw`text-white font-semibold text-base mx-1`]}>
+                    {weatherData.current.humidity}%
+                  </Text>
+                </View>
+                <View style={[tw`flex-row items-center space-x-2`]}>
+                  <Icon name="schedule" size={30} style={[tw`text-white`]} />
+                  <Text style={[tw`text-white font-semibold text-base mx-1`]}>
+                    {new Date(
+                      weatherData.current.last_updated,
+                    ).toLocaleTimeString('en-US', {
+                      hour: '2-digit',
+                      minute: '2-digit',
+                    })}
+                  </Text>
+                </View>
+              </View>
+              <ScrollView
+                horizontal={true}
+                contentContainerStyle={{paddingHorizontal: 15}}
+                showsHorizontalScrollIndicator={false}>
+                {weatherData.forecast.forecastday
+                  .splice(0, 7)
+                  .map((day, index) => (
+                    <View
+                      key={index} // Use a unique key prop
+                      style={[
+                        tw`flex justify-center items-center w-24 rounded-3xl my-4 py-2 px-1 mr-4`,
+                        {backgroundColor: 'rgba(255, 255, 255, 0.2)'},
+                      ]}>
+                      <Image
+                        source={{uri: `https:${day.day.condition.icon}`}}
+                        style={[tw`h-13 w-15`]}
+                      />
+                      <Text style={[tw`text-white py-1 text-center`]}>
+                        {new Date(day.date).toLocaleDateString('en-US', {
+                          weekday: 'short',
+                        })}
+                      </Text>
+                      <Text
+                        style={[
+                          tw`text-white text-xl font-semibold text-center`,
+                        ]}>
+                        {day.day.avgtemp_c}°C
+                      </Text>
+                    </View>
+                  ))}
+              </ScrollView>
             </View>
-
-            {/* Other Status */}
-            <View style={[tw`flex-row justify-between mx-4`]}>
-              <View style={[tw`flex-row items-center space-x-2`]}>
-                 <Icon name="air" size={30} style={[tw`text-white`]}/>
-                <Text style={[tw`text-white font-semibold text-base mx-1`]}>23km</Text>
-              </View>
-              <View style={[tw`flex-row items-center space-x-2`]}>
-                 <Icon name="water-drop" size={30} style={[tw`text-white`]}/>
-                <Text style={[tw`text-white font-semibold text-base mx-1`]}>23%</Text>
-              </View>
-              <View style={[tw`flex-row items-center space-x-2`]}>
-                 <Icon name="sunny" size={30} style={[tw`text-white`]}/>
-                <Text style={[tw`text-white font-semibold text-base mx-1`]}>6:05 AM</Text>
-              </View>
+          ) : (
+            <View style={[tw`flex h-full justify-center items-center`]}>
+              <Text style={[tw`text-4xl text-white font-bold`]}>Weather AppLication</Text>
             </View>
-        </View>
-
-        <View>
-          <View style={[tw`flex-row items-center mx-5 space-x-2`]}>
-            <Icon name="calendar-month" size={30} style={[tw`text-white`]}/>
-            <Text style={[tw`text-white mx-1 text-base`]}>Daily Forecast</Text>
-          </View>
-
-          <ScrollView
-          horizontal={true}
-          contentContainerStyle={{paddingHorizontal: 15}}
-          showsHorizontalScrollIndicator={false}>
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3 mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py- mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3 mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3 mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3  mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3 mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-
-          <View style={[tw`flex justify-center items-center w-24 rounded-3xl my-4 py-3 mr-4`, {backgroundColor: 'rgba(255, 255, 255, 0.2)'}]}>
-            <Image source={require("../images/thunder.png")} 
-            style={[tw`h-13 w-15`]}/>
-            <Text style={[tw`text-white py-1`]}>Monday</Text>
-            <Text style={[tw`text-white text-xl font-semibold text-center`]}>23°C</Text>
-          </View>
-          </ScrollView>
-        </View>
+          )}
+        </ScrollView>
       </SafeAreaView>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
